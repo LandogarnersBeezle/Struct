827804a34ac78587284e56c37a0e30d5274e5a23
LandogarnersBeezle <ottokiefer@icloud.com>
2026-05-01 19:08:37 +0200

    Initial Commit

---
+++ Struct/StructApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructApp.swift
+//  Struct
+//
+//  Created by Otto Kiefer on 01.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct StructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
