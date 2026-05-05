7687d158718ef9dc661d9787c7bd8d8292f61dbc
LandogarnersBeezle <ottokiefer@icloud.com>
2026-05-05 13:08:03 +0200

    Enhanced container structure, added container focus view

---
+++ Struct/ContainerRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ContainerRowView.swift
+//  Struct
+//
+//  Created by Otto Kiefer on 05.05.2026.
+//
+
+import SwiftUI
+
+struct ContainerRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContainerRowView()
+}
