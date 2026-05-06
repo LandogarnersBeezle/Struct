490b6e55cd0ba728586eaf1baffca6d1c7507ff8
LandogarnersBeezle <ottokiefer@icloud.com>
2026-05-06 16:28:35 +0200

    Refactoring

---
+++ Struct/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  Struct
+//
+//  Created by Otto Kiefer on 06.05.2026.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
